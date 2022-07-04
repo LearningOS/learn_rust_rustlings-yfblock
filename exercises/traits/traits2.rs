2409abc2a1a91190079022eb6ac74f0f9de9dba7
yufeng <321353225@qq.com>
2022-07-04 14:44:07 +0800

    complete traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,14 +10,18 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
-
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut ans = self.clone();
+        ans.push(String::from("Bar"));
+        ans
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
